cb9073258c0aa0524a3529c39c9bc54a4a3429ec
Vlad <sidorecvv@gmail.com>
2022-12-31 01:10:17 +0100

    add chart

--- src/App.tsx
+++ src/App.tsx
@@ -148,15 +148,28 @@ function App() {
     },
   };
 
-  const labels = [stock.time];
+  const labels = stockArr.map((stock) => stock.time);
+  const prices = stockArr.map((stock) => stock.price);
   const data = {
     labels,
     datasets: [
       {
         label: `${stockName} stock`,
-        data: stock.price,
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        data: prices,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
       },
     ],
   };
